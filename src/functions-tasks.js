4fcae4774e9db2caca4151c64ef3db3621f82a3d
Insspirit <kovshiktanya@gmail.com>
2025-04-14 00:41:40 +0300

    feat: the solution is complete

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -202,8 +202,11 @@ function logger(func, logFunc) {
  *   partialUsingArguments(fn, 'a','b','c')('d') => 'abcd'
  *   partialUsingArguments(fn, 'a','b','c','d')() => 'abcd'
  */
-function partialUsingArguments(/* fn, ...args1 */) {
-  throw new Error('Not implemented');
+function partialUsingArguments(fn, ...args1) {
+  const str = (...args) => {
+    return fn(...args1, ...args);
+  };
+  return str;
 }
 
 /**
@@ -223,8 +226,13 @@ function partialUsingArguments(/* fn, ...args1 */) {
  *   getId4() => 7
  *   getId10() => 11
  */
-function getIdGeneratorFunction(/* startFrom */) {
-  throw new Error('Not implemented');
+function getIdGeneratorFunction(startFrom) {
+  let newId = startFrom - 1;
+  const getId = () => {
+    newId += 1;
+    return newId;
+  };
+  return getId;
 }
 
 module.exports = {
